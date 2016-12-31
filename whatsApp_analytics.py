d87392fb04fc0ec5ea5ce34217066ff2f1bd2617
alston251 <alston2502000@yahoo.com.hk>
2016-12-31 23:46:48 +0800

    Added optional save and plot
    Added feature to make file export optional and to plot pie chart using
    matplotlib.

--- whatsApp_analytics.py
+++ whatsApp_analytics.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 
+# whatsApp_analytics.py
+# 31/12/2016
+# Alston Cheung
+
 import sys
 import re
+import matplotlib.pyplot as plt
 
 def find(parameter, str):
   result = re.findall(parameter, str)
@@ -39,17 +44,17 @@ def extract_names(filename):
     output = names_count.items()
     return output
 
-
 def main():
     args = sys.argv[1:]
 
     if not args:
-        print "usage: whatsApp_analytics.py [whatsApp _chat.txt file]"
+        print "usage: [whatsApp _chat.txt] [--tocsv optional] [output file]"
         sys.exit(1)
 
     # get path of chat.txt file
     txt_file = args[0]
 
+    # function for sorting list fo tuples by 2nd item
     def sort_tuple(each_word):
         return each_word[-1]
 
@@ -58,16 +63,38 @@ def main():
     result = sorted(result, key = sort_tuple, reverse = True)
     print result
 
-    # create output file
-    output_name = "output.csv"
-    output_file = open(output_name, "w")
+    # matplotlib parameters
+    labels = zip(*result)[0]
+    values = zip(*result)[1]
 
-    for each in result:
-        output_file.write(each[0] + "," + str(each[1]) + "\n")
-    output_file.close()
+    # generate custom matplotlb labels
+    def make_autopct(values):
+        def my_autopct(pct):
+            total = sum(values)
+            val = int(round(pct*total/100.0))
+            return '{p:.2f}%  ({v:d})'.format(p=pct,v=val)
+        return my_autopct
 
-    # output with matplotlib
+    # plot pie chart
+    plt.pie(values, labels = labels, shadow = True, autopct = make_autopct(values))
+    plt.style.use('seaborn-bright')
+    plt.title("Number of WhatsApp messages")
+    plt.axis('equal')
+    plt.show()
 
+    if len(args) > 1:
+        if args[1] == '--tocsv':
+            if len(args) < 3:
+                print "error: not enough command args"
+                sys.exit(1)
+
+            # create output file
+            output_name = args[2]
+            output_file = open(output_name, "w")
+
+            for each in result:
+                output_file.write(each[0] + "," + str(each[1]) + "\n")
+            output_file.close()
 
 if __name__ == '__main__':
     main()
